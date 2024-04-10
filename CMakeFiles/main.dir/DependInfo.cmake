70c3ee2fa94a59785ed2608c8fd1fc392ff44146
OakTree-42 <DannyGaev@gmail.com>
2024-04-09 23:31:56 -0500

    Added initial structures and files

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/oaktree/Documents/GitHub/c-bootcamp-DannyGaev/r_forth/general_stack.c" "CMakeFiles/main.dir/general_stack.c.o" "gcc" "CMakeFiles/main.dir/general_stack.c.o.d"
+  "/Users/oaktree/Documents/GitHub/c-bootcamp-DannyGaev/r_forth/int_stack.c" "CMakeFiles/main.dir/int_stack.c.o" "gcc" "CMakeFiles/main.dir/int_stack.c.o.d"
+  "/Users/oaktree/Documents/GitHub/c-bootcamp-DannyGaev/r_forth/main.c" "CMakeFiles/main.dir/main.c.o" "gcc" "CMakeFiles/main.dir/main.c.o.d"
+  "/Users/oaktree/Documents/GitHub/c-bootcamp-DannyGaev/r_forth/resolveActions.c" "CMakeFiles/main.dir/resolveActions.c.o" "gcc" "CMakeFiles/main.dir/resolveActions.c.o.d"
+  "/Users/oaktree/Documents/GitHub/c-bootcamp-DannyGaev/r_forth/token.c" "CMakeFiles/main.dir/token.c.o" "gcc" "CMakeFiles/main.dir/token.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
