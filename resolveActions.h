a77f5d88833c6ad15739d32a138221cab554c87a
OakTree-42 <DannyGaev@gmail.com>
2024-04-23 00:11:30 -0500

    Added headers to support cipher functionality

--- resolveActions.h
+++ resolveActions.h
@@ -4,9 +4,9 @@
 // int_stack_t resolveArith(char *answer, int_stack_t myIntStack);
 void resolveArith(char *answer, int_stack_t *myIntStack);
 
-void resolveWord(char *answer, int_stack_t *myIntStack, int textLength);
+void resolveWord(char *answer, int_stack_t *myIntStack, int textLength, char *token_array[]);
 
-void resolveSymbol(char *answer, int_stack_t *myIntStack, general_stack_t *myGeneralStack, int textLength, char *token_array[]);
+void resolveSymbol(char *answer, int_stack_t *mis, word_stack_t *mws, int textLength, char *token_array[]);
 
 void resolveVariable(char *answer, general_stack_t *myGeneralStack, int textLength);
 
