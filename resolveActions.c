ab23e5e5da12da61177003c015c0e5eaa826f3eb
OakTree-42 <DannyGaev@gmail.com>
2024-04-23 00:11:14 -0500

    Added support for cipher functionality

--- resolveActions.c
+++ resolveActions.c
@@ -1,6 +1,8 @@
 #include "int_stack.h"
 #include "general_stack.h"
+#include "word_stack.h"
 #include "token.h"
+#include "enc_ciph.h"
 #include <string.h>
 #include <stdlib.h>
 
@@ -23,13 +25,12 @@ void resolveArith(char *answer, int_stack_t *mis)
     }
 }
 
-void resolveWord(char *answer, int_stack_t *mis, int textLength)
+void resolveWord(char *answer, int_stack_t *mis, int textLength, char *token_array[])
 {
 
     char text[textLength + 1];
     strncpy(text, answer, textLength);
-    text[textLength - 1] = '\0';
-
+    text[textLength] = '\0';
     if (strcmp(text, "rot") == 0)
     {
         int_stack_rot(mis);
@@ -50,6 +51,30 @@ void resolveWord(char *answer, int_stack_t *mis, int textLength)
     {
         int_stack_over(mis);
     }
+    else if (strcmp(text, "ceasar") == 0)
+    {
+        char *els[20];
+        if (token_array[1] == NULL)
+        {
+            printf("?");
+        }
+        else
+        {
+            int x = 1;
+            char wordName[strlen(token_array[x])];
+            strncpy(wordName, token_array[x], strlen(token_array[x]));
+            while (strcmp(token_array[x], ";") != 0)
+            {
+                els[x - 1] = token_array[x];
+                token_array[x] = NULL;
+                els[x - 1][strcspn(els[x - 1], "\n")] = '\0';
+                x++;
+            }
+            els[x] = NULL;
+            int increment = atoi(&els[0][0]);
+            ceasarCipher(els, increment);
+        }
+    }
     else if (strcmp(text, "exit") == 0)
     {
         exit(0);
@@ -68,7 +93,7 @@ void resolveVariable(char *answer, general_stack_t *mgs, int textLength)
     }
 }
 
-void resolveSymbol(char *answer, int_stack_t *mis, general_stack_t *mgs, int textLength, char *token_array[])
+void resolveSymbol(char *answer, int_stack_t *mis, word_stack_t *mws, int textLength, char *token_array[])
 {
     switch (*answer)
     {
@@ -108,7 +133,7 @@ void resolveSymbol(char *answer, int_stack_t *mis, general_stack_t *mgs, int tex
                 x++;
             }
             els[x - 1] = NULL;
-            general_stack_word(mgs, els, wordName);
+            word_stack_declare(mws, els, wordName);
             break;
         }
     }
