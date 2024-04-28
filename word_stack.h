368ce53074af91d284077490949aa4250cb0fe1d
OakTree-42 <DannyGaev@gmail.com>
2024-04-28 00:02:01 -0500

    Added headers to suppurt new word declaration process

--- word_stack.h
+++ word_stack.h
@@ -4,32 +4,25 @@
 #include <stdio.h>
 #include <sys/queue.h>
 
-typedef struct word_t
+typedef struct word_entry
 {
     char *full_command;
     char *name;
-} WORD_T;
-
-
-typedef struct word_entry
-{
-    // char *value;
-    WORD_T word_t;
-    SLIST_ENTRY(word_entry) entries;
-} word_entry_t; 
+    SLIST_ENTRY(word_entry)
+    entries3;
+} word_entry_t;
 
 typedef struct word_stack
 {
-    SLIST_HEAD(stackhead3, word_entry) head3;
+    SLIST_HEAD(stackhead3, word_entry)
+    head3;
     int size;
     int capacity;
 } word_stack_t;
 
-
-
 extern void word_stack_init(word_stack_t *stk, int capacity);
-extern void word_stack_declare(word_stack_t *stk, char *els[], char wordName[]);
-extern int word_stack_push(word_stack_t *stk, WORD_T word_t);
+extern void word_stack_declare(word_stack_t *stk, char *els[]);
+extern int word_stack_push(word_stack_t *stk, word_entry_t word_t);
 extern void word_stack_print(word_stack_t *stk, FILE *file);
 
-#endif // WORD_STACK_H 
+#endif // WORD_STACK_H
