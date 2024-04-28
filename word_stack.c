7d195ef032f7d3410953bd739ff7c94dc86ac924
OakTree-42 <DannyGaev@gmail.com>
2024-04-28 00:01:48 -0500

    Updated process by which words are declared

--- word_stack.c
+++ word_stack.c
@@ -4,102 +4,86 @@
 #include <stdio.h>
 #include <string.h>
 
-void word_stack_init(word_stack_t *stk, int capacity)
+void word_stack_init(word_stack_t *stk3, int capacity)
 {
-    SLIST_INIT(&stk->head3);
-    stk->size = 0;
-    stk->capacity = capacity; 
+    SLIST_INIT(&stk3->head3);
+    stk3->size = 0;
+    stk3->capacity = capacity;
 }
 
-int word_stack_push(word_stack_t *stk, WORD_T word_t)
+int word_stack_push(word_stack_t *stk3, word_entry_t newWord_t)
 {
-    if (stk->size >= stk->capacity)
+    if (stk3->size >= stk3->capacity)
     {
-        printf("Stack is at full capacity.\n"); 
+        printf("Stack is at full capacity.\n");
         return 0; // fail
     }
 
     word_entry_t *newEntry = malloc(sizeof(word_entry_t));
     if (newEntry)
     {
-        newEntry->word_t.name = word_t.name;
-        newEntry->word_t.full_command = word_t.full_command;
-        SLIST_INSERT_HEAD(&stk->head3, newEntry, entries);
-        stk->size++;
+        newEntry->full_command = newWord_t.full_command;
+        newEntry->name = newWord_t.name;
+        printf("%s\n", newEntry->full_command);
+        SLIST_INSERT_HEAD(&stk3->head3, newEntry, entries3);
+        stk3->size++;
         return 1; // success
     }
     return 0; // fail
 }
 
-void word_stack_declare(word_stack_t *stk, char *els[], char wordName[])
+void word_stack_declare(word_stack_t *stk3, char *els[])
 {
     int size = 0;
-    int x = 0;
+    int x = 1;
+    int fullLength = 0;
+    word_entry_t newWord;
+    newWord.name = els[0];
     while (els[x] != NULL)
     {
+        fullLength += strlen(els[x]);
         size++;
         x++;
     }
-    printf("SIZE: %d\n", size);
-    char *fullCommand[size];
+    char *fullCommand[fullLength];
     x = 0;
-    while (x < size - 1)
+    while (x < fullLength)
     {
-        fullCommand[x] = els[x];
-        fullCommand[x][strcspn(fullCommand[x], "\n")] = '\0';
+        fullCommand[x] = els[x + 1];
         x++;
     }
-    // printf("Now pushing...");
     char space[] = " ";
-    char full[20] = "\0";
-    for (int i = 0; i < size - 1; i++)
+    char full[100] = "\0";
+    for (int i = 0; i < fullLength; i++)
     {
         strcat(full, fullCommand[i]);
-        strcat(full, space); 
+        strcat(full, space);
     }
-    WORD_T newWord;
     newWord.full_command = full;
-    newWord.name = wordName;
+    printf("NEWWORD FULL COMMAND: %s\n", newWord.full_command);
+    printf("NEWWORD NAME: %s\n", newWord.name);
 
-    int pushed = word_stack_push(stk,newWord);
-    if(pushed)
+    int pushed = word_stack_push(stk3, newWord);
+    if (pushed)
     {
-        printf("Succesfully declared word: %s\n", wordName);
+        printf("Succesfully declared word: %s\n", newWord.name);
     }
-    
 }
 
-void word_stack_print(word_stack_t *stk, FILE *file)
+void word_stack_print(word_stack_t *stk3, FILE *file)
 {
-    fprintf(file, "\n\tSTACK\n\t|");
+    fprintf(file, "\n\tWORD STACK\n");
     word_entry_t *entry;
-    int length = stk->size;
-    if (stk->size == 0)
+    int length = stk3->size;
+    if (stk3->size == 0)
     {
         fprintf(file, "empty stack\n");
         return;
     }
 
-    char *elems = (char *)malloc(length * sizeof(char));
-
-    SLIST_FOREACH(entry, &stk->head3, entries)
+    SLIST_FOREACH(entry, &stk3->head3, entries3)
     {
-        char str[sizeof(entry->word_t.name)+sizeof(entry->word_t.full_command)];
-        printf("NAME: %s\tFULL COMMAND: %s\n", entry->word_t.name,entry->word_t.full_command);
-        strcat(elems, str);
-    }
-
-    char *token = strtok(elems, " \t\n");
-    char *tokens[100];
-    int count = 0;
-    while (token != NULL)
-    {
-        tokens[count++] = token;
-        token = strtok(NULL, " ");
-    }
-
-    for (int i = count - 1; i >= 0; i--)
-    {
-        fprintf(file, "%s ", tokens[i]);
+        printf("COMMAND: %s\n", entry->full_command);
+        printf("\t|NAME: %s\tFULL COMMAND: %s\n", entry->name, entry->full_command);
     }
 }
